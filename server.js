0e5f876eb5165357527907125a408f5bf08419c3
tim minker <nigel8181@gmail.com>
2022-05-27 16:31:16 -0400

    connecte to atlas

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const Plants = require('./models/plants.js')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-// routes
 
-app.listen(3000, () => {
-  console.log('listening on port 3000...');
+
+// routes
+app.post('/plants', (req, res) => {
+  Plants.create(req.body, (err, addPlant) => {
+    res.json(addPlant)
+  })
 })
 
-mongoose.connect('mongodb://localhost:27017/')
+app.get('/plants', (req, res) => {
+  Plants.find({}, (err, foundPlant) => {
+    res.json(foundPlant)
+  })
+})
+
+app.delete('/plants/:id', (req, res) => {
+  Plants.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
+    res.json(deletedPlant)
+  })
+})
+
+app.put('/plants/:id', (req, res) => {
+  Plants.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPlant) => {
+    res.json(updatedPlant)
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log('listening on port: 3000');
+})
+
+mongoose.connect(MONGODB_URI, () => {
+  console.log('connected to mongo');
+})
 
 mongoose.connection.once('open', () => {
   console.log('connected to mongod...');
