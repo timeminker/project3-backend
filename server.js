264ee244f4f4c8fb4bee7b9c067c2fad8a37b1bb
CarolineDuffett <ckduffett@gmail.com>
2022-05-27 14:42:01 -0400

    changed env uri

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+//___________________
+//Dependencies
+//___________________
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
-
 const app = express()
+require('dotenv').config()
+
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT
+
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI
+
+//___________________
+//Middleware
+//___________________
 app.use(express.json())
 app.use(cors())
 
 // routes
 
-app.listen(3000, () => {
-  console.log('listening on port 3000...');
-})
+
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
 mongoose.connect('mongodb://localhost:27017/')
 
