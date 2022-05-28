0184e96571ff85b308806df147ff40923d4d2a9b
CarolineDuffett <ckduffett@gmail.com>
2022-05-28 10:08:48 -0400

    server.js

--- server.js
+++ server.js
@@ -4,14 +4,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
-const Plants = require('./models/plants.js')
-
-require('dotenv').config()
-
-const PORT = process.env.PORT
-
-const MONGODB_URI = process.env.MONGODB_URI
-
 const app = express()
 require('dotenv').config()
 
@@ -33,46 +25,15 @@ const MONGODB_URI = process.env.MONGODB_URI
 app.use(express.json())
 app.use(cors())
 
-
-
 // routes
-app.post('/plants', (req, res) => {
-  Plants.create(req.body, (err, addPlant) => {
-    res.json(addPlant)
-  })
-})
 
-app.get('/plants', (req, res) => {
-  Plants.find({}, (err, foundPlant) => {
-    res.json(foundPlant)
-  })
-})
-
-app.delete('/plants/:id', (req, res) => {
-  Plants.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
-    res.json(deletedPlant)
-  })
-})
-
-app.put('/plants/:id', (req, res) => {
-  Plants.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPlant) => {
-    res.json(updatedPlant)
-  })
-})
 
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+app.listen(PORT, () => console.log( 'Listening on port:', MONGODB_URI));
 
-
-app.listen(PORT, () => {
-  console.log('listening on port: 3000');
-})
-
-mongoose.connect(MONGODB_URI, () => {
-  console.log('connected to mongo');
-})
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
 
 mongoose.connection.once('open', () => {
   console.log('connected to mongod...');
