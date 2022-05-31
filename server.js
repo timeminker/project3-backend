6ad4aa4b59b99c762f3ac56c95f9d7d863971656
tim minker <nigel8181@gmail.com>
2022-05-31 18:12:43 -0400

    another CORS try

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const PROJECT3_DB = process.env.PROJECT3_DB
 app.use(express.json())
 app.use(cors())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", 'https://shrouded-wave-73322.herokuapp.com/');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
