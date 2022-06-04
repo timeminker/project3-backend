a82e660afe392a2c3d7bfed3dbd94b88d1ba2319
tim minker <nigel8181@gmail.com>
2022-06-04 12:17:02 -0400

    adding sessions

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const Plants = require('./models/plants.js')
 const Notes = require('./models/notes.js')
 require('dotenv').config()
 const app = express()
+const session = require('express-session')
 
 // controllers
-const plantsController = require('./controllers/all_plants.js')
+const sessionController = require('./controllers/sessions_controller.js')
 const notesController = require('./controllers/notes.js')
 const userController = require('./controllers/user_controller.js')
 const userPlantsController = require('./controllers/user_plants.js')
@@ -34,10 +35,19 @@ const PROJECT3_DB = process.env.PROJECT3_DB
 //___________________
 app.use(express.json())
 app.use(cors())
-// app.use(plantsController)
+app.use(
+  session({
+    secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+    resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+    saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+  })
+)
+
+// routes
 app.use(notesController)
 app.use(userPlantsController)
 app.use("/", userController)
+app.use(sessionController)
 
 
 // redirect for heroku
@@ -51,8 +61,8 @@ app.use("/", userController)
 app.listen(PORT, () => console.log('Listening on port: 3000'));
 
 // Connect to Mongo
-// mongoose.connect(PROJECT3_DB  ,  { useNewUrlParser: true});
-mongoose.connect("mongodb://localhost:27017/plants")
+mongoose.connect(PROJECT3_DB  ,  { useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/plants")
 
 // Error / success
 mongoose.connection.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
